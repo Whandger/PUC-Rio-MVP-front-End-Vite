63d990aa5db86a69d9fd0269c99c59da4f247b83
Whandger <whandgerw@yahoo.com.br>
2026-05-12 10:35:15 -0300

    Fix: adiciona basename no React Router

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/PUC-Rio-MVP-front-End-Vite">
       <App />
     </BrowserRouter>
   </StrictMode>
