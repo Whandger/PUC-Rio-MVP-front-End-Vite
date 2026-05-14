2b95d8a461ecffc3ee735de06a7d701bb5266daa
Whandger <whandgerw@yahoo.com.br>
2026-05-13 22:53:05 -0300

    Reseta o formulario ao fechar ele

--- src/pages/TreinoPage.tsx
+++ src/pages/TreinoPage.tsx
@@ -7,24 +7,31 @@ import { useTrainings } from "../hooks/useTrainings";
 
 export default function TreinoPage() {
   const [showForm, setShowForm] = useState(false);
+  // forçar a recriação do formulário
+  const [formKey, setFormKey] = useState(0);
 
-  const { trainings, addTraining, deleteTraining, updateExercise } =
-    useTrainings();
+  const { trainings, addTraining, deleteTraining, updateExercise } = useTrainings();
 
   async function handleAddTraining(data: any) {
     const success = await addTraining(data);
-
     if (success) {
       setShowForm(false);
+      setFormKey(prev => prev + 1); // Reseta o formulário
     }
   }
+
+  const handleCancelForm = () => {
+    setShowForm(false);
+    setFormKey(prev => prev + 1); // Reseta o formulário ao cancelar
+  };
 
   return (
     <div className="flex flex-col items-center gap-4 py-4">
       <AddTrainingForm
+        key={formKey}
         visible={showForm}
         onAdd={handleAddTraining}
-        onCancel={() => setShowForm(false)}
+        onCancel={handleCancelForm}
         onToggle={() => setShowForm((prev) => !prev)}
       />
       <TrainingList
